4fad0f59df634a39df3d235e89c6151e49b5f99b
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-10-16 01:27:20 +0100

    Modified css files.

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,7 +1,7 @@
 module FlightsHelper
 
   def get_user_image(user_image)
-      image_tag(user_image) if user_image
+      image_tag(user_image, class: "circle responsive-img") if user_image
   end
 
   def current_user
