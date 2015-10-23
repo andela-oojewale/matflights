d5adf58a4ba44e2ce8b7f23cce27098ad8678270
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-10-23 12:19:48 +0100

    Add "add passenger functionality"

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -11,9 +11,12 @@ $(document).ready(function(){
     var textbox1 = $('<input type="text">').attr({name: "name", class: "validate"});
     var label2 = $("<label>").text("Email");
     var textbox2 = $('<input type="text">').attr({name: "email", class: "validate"});
+    init_total =  parseInt($(".total").text().match(/\d+/))
+    var total = init_total + parseInt($(".add_more").val())
     var rem = $('<button />').attr({class: "remove_pass btn blue", type: "button"});
     rem.text("Remove passenger");
     $(".pass_list").append(label1, textbox1, label2, textbox2, rem);
+    $(".total").text("Total Cost: $" + total)
   }
 
   // function remove_element()
