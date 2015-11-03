a300ae9493ccbdde9ff892281712442a5a737a1d
ANDELA UBUNTU <ojewaleolaide@gmail.com>
2015-11-03 18:21:00 +0100

    Modify feature spec

--- spec/features/flights_spec.rb
+++ spec/features/flights_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "Flights", type: :feature do
-
+  before do
+    Capybara.default_driver = :selenium
+    OmniAuth.config.test_mode = true
+  end
   scenario "visit" do
     visit "/"
     expect(page).to have_title "MatFlights | Booking at ease"
@@ -15,15 +18,15 @@ RSpec.feature "Flights", type: :feature do
 
   end
 
-  # scenario "find no flight where departure and destination are not chosen." do
-  #   visit "/"
+  scenario "find no flight where departure and destination are not chosen." do
+    visit "/"
 
-  #   select "Select Departure Airport", from: "flight_from_id"
-  #   select "Select Destination Airport", from: "flight_to_id"
-  #   select "1", from: "passengers"
-  #   click_button "Search Flight"
-  #   expect(page).to have_content("Select your departure and destination airports.")
-  # end
+    select "Select Departure Airport", from: "flight_from_id"
+    select "Select Destination Airport", from: "flight_to_id"
+    select "1", from: "passengers"
+    click_button "Search Flight"
+    expect(page).to have_content("Select your departure and destination airports.")
+  end
 
   # scenario "find no flight where departure and destination match no flight." do
   #   visit "/"
